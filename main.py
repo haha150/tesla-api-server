e541ff802aaf5a0d670a30af1e2aa0c296cacc0c
Ali Symeri <alisymeri@Alis-MBP.lan>
2023-01-29 13:11:54 +0100

    update api

--- main.py
+++ main.py
@@ -421,4 +421,4 @@ def set_charge_limit(id):
 
 if __name__ == '__main__':
     #app.run(debug=True, host='0.0.0.0', port=5000)
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ from main import app
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', port=443, ssl_context=('fullchain.pem', 'privkey.pem'))
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
